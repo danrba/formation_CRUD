b0d350f2d71b2b7be095b508ae3fe4450bee013a
dan.su <dan.su@POSTE0018>
2020-04-15 09:03:33 -0400

    CRUD_1Page_Framework: equipe.component

--- CRUD_1Page_Framework/angular/src/app/equipes/equipe/equipe.component.ts
+++ CRUD_1Page_Framework/angular/src/app/equipes/equipe/equipe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EquipeService } from 'src/app/shared/equipe.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-equipe',
@@ -11,6 +12,24 @@ export class EquipeComponent implements OnInit {
   constructor( public service : EquipeService ) { }
 
   ngOnInit(): void {
+    this.resetForm();
   }
 
+  resetForm(form?: NgForm) {
+    if (form != null)
+      form.resetForm();
+
+    this.service.formData = {
+      EquipeId: null,
+      Nom:'',
+      Ville:'',
+      Sport:''
+    }
+
+  }
+
+  onSubmit(form : NgForm) {
+
+  }
+  
 }
